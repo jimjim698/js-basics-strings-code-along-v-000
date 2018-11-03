e688f21338e7b689fba6ada97db49c9352577c40
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-03 23:12:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = ` Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser[0]}`;
